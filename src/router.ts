355c6fa3804c4e69f8565c8aadf602001af7ad8c
Qmo <qmo.chiu@baifu-tech.net>
2019-06-17 18:14:11 +0800

    json-server & setup & router update

--- src/router.ts
+++ src/router.ts
@@ -16,10 +16,17 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/playlist',
+      name: 'playlist',
+      component: () => import(/* webpackChunkName: "playlist" */ './views/Playlist.vue')
+    },
+    {
+      path: '*',
+      name: 'error',
+      component: () => import(/* webpackChunkName: "error" */ './views/Error.vue')
     }
   ]
 })
